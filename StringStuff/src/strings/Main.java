d5652a1ec8ab31abd0edd1be9ab8efe5db3d5ea3
silvesam <silvesam@LCBLAB2110-35.uccob.uc.edu>
2019-12-03 09:10:39 -0500

    finished

--- StringStuff/src/strings/Main.java
+++ StringStuff/src/strings/Main.java
@@ -1,6 +1,7 @@
 /*
- * Bill Nicholson
- * nicholdw@ucmail.uc.edu
+ * Aaron Silvestri	
+ * silvesam@mail.uc.edu
+ * App Dev with Java
  * Demonstrating the String and StringBuilder data types
  */
 package strings;
@@ -13,11 +14,12 @@ public class Main {
 	public static void testSomeStrings() {
 		String test = "Todd Lindemann";
 		long startTime = System.nanoTime();
-		for (int i = 0; i < 1000000000; i++) {
-
+		for (int i = 0; i < 1000000000; i++) { //One billion interations
+			String fruit = "Applee";
+			fruit = fruit.replace("ee", "e");
 		}
 		long endTime = System.nanoTime();
-
+		
 		// get difference of two nanoTime values
 		long timeElapsed = endTime - startTime;
 		System.out.println("testSomeStrings(): Execution time in milliseconds : " + (double)timeElapsed / 1000000);
@@ -29,7 +31,11 @@ public class Main {
 		StringBuilder test = new StringBuilder("Todd Lindemann");
 		long startTime = System.nanoTime();
 		for (int i = 0; i < 1000000000; i++) {
-
+			test = new StringBuilder("Applee");
+			//System.out.println("Before replace, test = " + test.toString()) ;
+			test.replace(4,7, "e"); //replace ee with e
+			//System.out.println("After replace, test = " + test.toString()) ;
+			//System.out.println("");
 		}
 		long endTime = System.nanoTime();
 
